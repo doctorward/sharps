625ba4fb44befe96b7b552f141dd818a51647897
Ryan Dorward <ryandorward@gmail.com>
2020-10-02 12:12:59 -0700

    throttle and streamline js scroll effect

--- js/sharps.js
+++ js/sharps.js
@@ -1,24 +1,37 @@
 
 window.onLoad = function() {        
   window.scrollTo(0,1);
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
-window.onscroll = function(){
+var logo = document.getElementById('sharps-logo');
+var height = window.innerHeight;  
 
-  const scrollTop = window.scrollY;
-  const height = window.innerHeight;   
+function scrollyMagic() {
+  const scrollTop = window.scrollY;   
   const fromTop = height - (scrollTop*2);  
+  const percent = 100*fromTop/height; 
 
   if (fromTop <= 0) 
-    document.getElementById('sharps-logo').style.width = "0%";
-  else  {
-    const percent = 100*fromTop/height;      
-    document.getElementById('sharps-logo').style.width = percent + "%";
-  }  
-           
-};
+    logo.style.width = "0%";
+  else
+    logo.style.width = percent + "%";   
+}
 
-window.scroll();
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+}
+
+window.addEventListener('scroll', throttle(scrollyMagic, 40));
+
+// window.scroll();
+window.scrollTo(0,1);
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
@@ -29,4 +42,5 @@ window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  height = window.innerHeight;  
 });
